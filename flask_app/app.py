a8aea3712076bb263e905507ca2068f2123e2f59
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-26 21:06:32 +0200

    pinnig the app to templates and static files

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return '<h1>Welcome!<h1>'
+    return render_template('welcome.html')
 
