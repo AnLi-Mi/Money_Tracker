22eb381549f4de65f5a5d42baaf5f91f2d52876a
AnLi-Mi <a.lipiarz@gmail.com>
2020-08-26 21:30:29 +0200

    connecting to MySQL database

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
 
+from flaskext.mysql import MySQL
+
 app=Flask(__name__)
+mysql=MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '8G13rm3k'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route('/')
 def welcome():
@@ -17,3 +25,20 @@ def see_all():
 @app.route('/monthlysummary')
 def monthly_summary():
     return render_template('monthlysummary.html')
+
+
+def connect_msql():
+    conn = mysql.connect()
+    if (conn):
+    # Carry out normal procedure
+        print ("Connection successful")
+    else:
+    # Terminate
+        print ("Connection unsuccessful")
+
+
+connect_msql()
+
+
+
+
